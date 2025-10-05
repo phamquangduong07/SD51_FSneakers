3a79ccd6820268cd762d3a72a35b0ccf1e3e8847
duongpham07 <phamquangduong07@gmail.com>
2025-10-05 23:27:23 +0700

    Create Project

---
+++ src/test/java/com/sd51/fsneaker/FSneakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sd51.fsneaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FSneakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
