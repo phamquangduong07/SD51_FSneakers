239c403c3d2b6897347c37a2b1c089a92423122a
duongpham07 <phamquangduong07@gmail.com>
2025-10-17 15:51:58 +0700

    Create project

---
+++ src/main/java/com/sd51/fsneakers/FsneakersApplication.java
@@ -0,0 +1,13 @@
+package com.sd51.fsneakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsneakersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FsneakersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sd51/fsneakers/FsneakersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sd51.fsneakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsneakersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
