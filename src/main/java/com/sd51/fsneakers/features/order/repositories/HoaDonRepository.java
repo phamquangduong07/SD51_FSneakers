e3afe03cb5128b060703fbe1df2907bc806b065b
duongpham07 <phamquangduong07@gmail.com>
2025-10-21 19:38:45 +0700

    update entity order

--- src/main/java/com/sd51/fsneakers/features/order/repositories/HoaDonRepository.java
+++ src/main/java/com/sd51/fsneakers/features/order/repositories/HoaDonRepository.java
@@ -1,5 +1,6 @@
 package com.sd51.fsneakers.features.order.repositories;
 
+import com.sd51.fsneakers.features.order.entity.HoaDon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
